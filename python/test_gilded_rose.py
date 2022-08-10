bd44227983503c9d15f6c498a5e2595186f27341
Nathan Chandler-Smith <nchandlersmith@verticalrelevance.com>
2022-08-10 14:35:39 -0500

    Tests update_quality decrements by 1 when expired.

--- python/test_gilded_rose.py
+++ python/test_gilded_rose.py
@@ -37,3 +37,15 @@ class TestGildedRose:
         result = app.items[0]
 
         assert result.sell_in == expected_sell_in
+
+    def test_update_quality_decrements_sell_in_by_1_when_expired(self):
+        starting_sell_in = -1
+        expected_sell_in = -2
+        items = [Item(NORMAL_ITEM_NAME, starting_sell_in, 10)]
+        app = GildedRose(items)
+
+        app.update_quality()
+
+        result = app.items[0]
+
+        assert result.sell_in == expected_sell_in
