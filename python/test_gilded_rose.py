0beb4b7a07cca7002aa5b03737327046befb57d4
Nathan Chandler-Smith <nchandlersmith@verticalrelevance.com>
2022-08-10 15:08:20 -0500

    Tests update_quality does not change the name of Aged Brie.

--- python/test_gilded_rose.py
+++ python/test_gilded_rose.py
@@ -1,6 +1,7 @@
 from python.gilded_rose import GildedRose, Item
 
 NORMAL_ITEM_NAME = "normal"
+AGED_BRIE_NAME = "Aged Brie"
 
 
 class TestGildedRose:
@@ -127,3 +128,13 @@ class TestGildedRose:
         result = app.items[0]
 
         assert result.quality == expected_quality
+
+    def test_update_quality_aged_brie_does_not_change_normal_item_name(self):
+        items = [Item(AGED_BRIE_NAME, 10, 10)]
+        app = GildedRose(items)
+
+        app.update_quality()
+
+        result = app.items[0]
+
+        assert result.name == AGED_BRIE_NAME
