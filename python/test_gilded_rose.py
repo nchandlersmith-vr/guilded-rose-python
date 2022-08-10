f561977c0c70788e918e7f9435fc39ed4e3edf95
Nathan Chandler-Smith <nchandlersmith@verticalrelevance.com>
2022-08-10 15:44:42 -0500

    Tests update_quality for aged brie cannot exceed max when expires today.

--- python/test_gilded_rose.py
+++ python/test_gilded_rose.py
@@ -253,3 +253,29 @@ class TestGildedRose:
         result = app.items[0]
 
         assert result.quality == expected_quality
+
+    def test_update_quality_aged_brie_quality_cannot_exceed_max_when_expires_today_starting_quality_at_max(self):
+        sell_in = 0
+        starting_quality = MAX_ITEM_QUALITY
+        expected_quality = MAX_ITEM_QUALITY
+        items = [Item(AGED_BRIE_NAME, sell_in, starting_quality)]
+        app = GildedRose(items)
+
+        app.update_quality()
+
+        result = app.items[0]
+
+        assert result.quality == expected_quality
+
+    def test_update_quality_aged_brie_quality_cannot_exceed_max_when_expires_today_starting_quality_at_max_minus_1(self):
+        sell_in = 0
+        starting_quality = MAX_ITEM_QUALITY - 1
+        expected_quality = MAX_ITEM_QUALITY
+        items = [Item(AGED_BRIE_NAME, sell_in, starting_quality)]
+        app = GildedRose(items)
+
+        app.update_quality()
+
+        result = app.items[0]
+
+        assert result.quality == expected_quality
