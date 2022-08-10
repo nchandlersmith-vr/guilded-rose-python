29fe16edec54a7be099134b7deb4c45cb097d285
Nathan Chandler-Smith <nchandlersmith@verticalrelevance.com>
2022-08-10 16:38:03 -0500

    Adds test update quality for backstage passes increases quality by 2 when 10 days til concert.

--- python/test_gilded_rose.py
+++ python/test_gilded_rose.py
@@ -465,3 +465,16 @@ class TestGildedRose:
         result = app.items[0]
 
         assert result.quality == expected_quality
+
+    def test_update_quality_backstage_passes_increases_quality_by_1_when_10_days_til_concert(self):
+        sell_in = 10
+        starting_quality = 20
+        expected_quality = 22
+        items = [Item(BACKSTAGE_PASSES_NAME, sell_in, starting_quality)]
+        app = GildedRose(items)
+
+        app.update_quality()
+
+        result = app.items[0]
+
+        assert result.quality == expected_quality
