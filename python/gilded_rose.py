8e57baeadbb40cd5a69125e02d6c4162892a2931
Nathan Chandler-Smith <nchandlersmith@verticalrelevance.com>
2022-08-10 18:00:16 -0500

    Refactors normal items

--- python/gilded_rose.py
+++ python/gilded_rose.py
@@ -30,46 +30,13 @@ class GildedRose(object):
                 item.quality = min(item.quality, 50)
                 item.sell_in -= 1
                 break
-            if self._item_is_normal(item):
-                self._safe_degrade_quality(item)
             else:
-                self._backstage_pass_rules(item)
-            self._safe_decrement_sell_in(item)
-            self._expiration_rules(item)
-
-    def _expiration_rules(self, item):
-        if item.sell_in < 0:
-            if item.name != "Aged Brie":
-                if item.name != "Backstage passes to a TAFKAL80ETC concert":
-                    if item.quality > 0:
-                        item.quality = item.quality - 1
-                else:
-                    item.quality = item.quality - item.quality
-            else:
-                if item.quality < 50:
-                    item.quality = item.quality + 1
-
-    def _backstage_pass_rules(self, item):
-        if item.quality < 50:
-            item.quality = item.quality + 1
-            if item.name == "Backstage passes to a TAFKAL80ETC concert":
-                if item.sell_in < 11:
-                    if item.quality < 50:
-                        item.quality = item.quality + 1
-                if item.sell_in < 6:
-                    if item.quality < 50:
-                        item.quality = item.quality + 1
-
-    def _safe_decrement_sell_in(self, item):
-        item.sell_in = item.sell_in - 1
-
-    def _safe_degrade_quality(self, item):
-        if item.quality > 0:
-            item.quality = item.quality - 1
-
-    def _item_is_normal(self, item):
-        return item.name != "Aged Brie" \
-               and item.name != "Backstage passes to a TAFKAL80ETC concert"
+                item.quality -= 1
+                if item.sell_in < 1:
+                    item.quality -= 1
+                item.quality = max(item.quality, 0)
+                item.sell_in -= 1
+                break
 
 
 class Item:
