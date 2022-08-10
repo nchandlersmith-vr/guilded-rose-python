c6a9e64574afb7aab69609f399b8e852f9f0438a
Nathan Chandler-Smith <nchandlersmith@verticalrelevance.com>
2022-08-10 16:15:37 -0500

    Adds test upgrade_quality on sulfurus does not degrade when not expired.

--- python/test_gilded_rose.py
+++ python/test_gilded_rose.py
@@ -353,3 +353,16 @@ class TestGildedRose:
         result = app.items[0]
 
         assert result.sell_in == expected_sell_in
+
+    def test_update_quality_sulfuras_does_not_degrade_when_not_expired(self):
+        starting_sell_in = 1
+        starting_quality = 80
+        expected_quality = 80
+        items = [Item(SULFURAS_NAME, starting_sell_in, starting_quality)]
+        app = GildedRose(items)
+
+        app.update_quality()
+
+        result = app.items[0]
+
+        assert result.quality == expected_quality
