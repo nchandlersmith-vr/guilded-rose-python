db5114896d95e1273efbd65fdfe54a7eb8792d74
Nathan Chandler-Smith <nchandlersmith@verticalrelevance.com>
2022-08-10 14:24:09 -0500

    Update quality does not change normal item name.

--- python/test_gilded_rose.py
+++ python/test_gilded_rose.py
@@ -1,4 +1,15 @@
-class TestGildedRose:
+from python.gilded_rose import GildedRose, Item
 
-    def test_true_is_true(self):
-        assert True is True
+NORMAL_ITEM_NAME = "normal"
+
+
+class TestGildedRose:
+    def test_update_quality_does_not_change_normal_item_name(self):
+        items = [Item(NORMAL_ITEM_NAME, 1, 10)]
+        app = GildedRose(items)
+
+        app.update_quality()
+
+        result = app.items[0]
+
+        assert result.name == NORMAL_ITEM_NAME
