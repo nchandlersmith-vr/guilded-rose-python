38e887a62551815cf222995e4e342035b774f896
Nathan Chandler-Smith <nchandlersmith@verticalrelevance.com>
2022-08-10 21:45:51 -0500

    Refactor aged brie evaluator.

--- python/item_evaluator/aged_brie_evaluator.py
+++ python/item_evaluator/aged_brie_evaluator.py
@@ -3,9 +3,18 @@ from python.item_evaluator.item_evaluator import ItemEvaluator
 from python.exceptions.exceptions import CannotEvaluateNonItemException, CannotEvaluateItemException
 
 
+def _validate_item(item):
+    if not isinstance(item, Item):
+        raise CannotEvaluateNonItemException(item)
+    if item.name != AgedBrieEvaluator.AGED_BRIE_ITEM_NAME:
+        raise CannotEvaluateItemException(AgedBrieEvaluator.AGED_BRIE_ITEM_NAME, item.name)
+
+
 class AgedBrieEvaluator(ItemEvaluator):
+    AGED_BRIE_ITEM_NAME = "Aged Brie"
+
     def evaluate(self, item: Item) -> Item:
-        self._validate_item(item)
+        _validate_item(item)
         if item.sell_in > 0:
             item.quality += 1
         else:
@@ -13,9 +22,3 @@ class AgedBrieEvaluator(ItemEvaluator):
         item.quality = min(item.quality, 50)
         item.sell_in -= 1
         return item
-
-    def _validate_item(self, item):
-        if not isinstance(item, Item):
-            raise CannotEvaluateNonItemException(item)
-        if item.name != "Aged Brie":
-            raise CannotEvaluateItemException("Aged Brie", item.name)
