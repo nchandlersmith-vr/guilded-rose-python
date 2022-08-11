d2e2257a6f8480bc73e34d72028ec170dc2988ed
Nathan Chandler-Smith <nchandlersmith@verticalrelevance.com>
2022-08-11 07:52:09 -0500

    ItemEvaluatorFactory creates backstage passes evaluator.

--- python/item_evaluator_factories/item_evaluator_factory.py
+++ python/item_evaluator_factories/item_evaluator_factory.py
@@ -1,5 +1,6 @@
 from python.item_evaluator_factories.item_evaluator_factory_abstract_class import ItemEvaluatorFactoryAbstractClass
 from python.item_evaluators.aged_brie_evaluator import AgedBrieEvaluator
+from python.item_evaluators.backstage_passes_evaluator import BackstagePassesEvaluator
 from python.item_evaluators.item_evaluator import ItemEvaluator
 from python.item_evaluators.sulfuras_evaluator import SulfurasEvaluator
 from python.exceptions.exceptions import UnsupportedItem
@@ -8,10 +9,13 @@ from python.exceptions.exceptions import UnsupportedItem
 class ItemEvaluatorFactory(ItemEvaluatorFactoryAbstractClass):
     SULFURUS_ITEM_NAME = "Sulfuras, Hand of Ragnaros"
     AGED_BRIE_ITEM_NAME = "Aged Brie"
+    BACKSTAGE_PASSES = "Backstage passes to a TAFKAL80ETC concert"
 
     def create(self, name: str) -> ItemEvaluator:
         if name == ItemEvaluatorFactory.SULFURUS_ITEM_NAME:
             return SulfurasEvaluator()
         if name == ItemEvaluatorFactory.AGED_BRIE_ITEM_NAME:
             return AgedBrieEvaluator()
+        if name == ItemEvaluatorFactory.BACKSTAGE_PASSES:
+            return BackstagePassesEvaluator()
         raise UnsupportedItem(name)
--- python/item_evaluator_factories/tests/test_item_evaluator_factory.py
+++ python/item_evaluator_factories/tests/test_item_evaluator_factory.py
@@ -2,6 +2,7 @@ import pytest
 
 from python.exceptions.exceptions import UnsupportedItem
 from python.item_evaluator_factories.item_evaluator_factory import ItemEvaluatorFactory
+from python.item_evaluators.backstage_passes_evaluator import BackstagePassesEvaluator
 from python.item_evaluators.sulfuras_evaluator import SulfurasEvaluator
 from python.item_evaluators.aged_brie_evaluator import AgedBrieEvaluator
 
@@ -16,14 +17,15 @@ class TestItemEvaluatorFactory:
 
     def test_create_creates_a_sulfurus_evaluator(self):
         factory = ItemEvaluatorFactory()
-
-        item = factory.create("Sulfuras, Hand of Ragnaros")
-
-        assert isinstance(item, SulfurasEvaluator)
+        evaluator = factory.create("Sulfuras, Hand of Ragnaros")
+        assert isinstance(evaluator, SulfurasEvaluator)
 
     def test_create_creates_a_aged_brie_evaluator(self):
         factory = ItemEvaluatorFactory()
+        evaluator = factory.create("Aged Brie")
+        assert isinstance(evaluator, AgedBrieEvaluator)
 
-        item = factory.create("Aged Brie")
-
-        assert isinstance(item, AgedBrieEvaluator)
+    def test_create_creates_a_backstage_passes_evaluator(self):
+        factory = ItemEvaluatorFactory()
+        evaluator = factory.create("Backstage passes to a TAFKAL80ETC concert")
+        assert isinstance(evaluator, BackstagePassesEvaluator)
