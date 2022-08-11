ed4f9bed9b158e6df4e4200e5ae6b7f2590ec5f7
Nathan Chandler-Smith <nchandlersmith@verticalrelevance.com>
2022-08-11 08:14:24 -0500

    gilded_rose.py uses evaluator factory.

--- python/gilded_rose.py
+++ python/gilded_rose.py
@@ -1,8 +1,6 @@
 # -*- coding: utf-8 -*-
-from python.item_evaluators.sulfuras_evaluator import SulfurasEvaluator
-from python.item_evaluators.aged_brie_evaluator import AgedBrieEvaluator
-from python.item_evaluators.backstage_passes_evaluator import BackstagePassesEvaluator
-from python.item_evaluators.normal_item_evaluator import NormalItemEvaluator
+from python.item import Item
+from python.item_evaluator_factories.item_evaluator_factory import ItemEvaluatorFactory
 
 
 class GildedRose(object):
@@ -10,17 +8,11 @@ class GildedRose(object):
     AGED_BRIE = "Aged Brie"
     BACKSTAGE_PASSES = "Backstage passes to a TAFKAL80ETC concert"
 
-    def __init__(self, items):
+    def __init__(self, items: [Item]):
         self.items = items
-        self.evaluator_lookup = {
-            GildedRose.SULFURUS: SulfurasEvaluator(),
-            GildedRose.AGED_BRIE: AgedBrieEvaluator(),
-            GildedRose.BACKSTAGE_PASSES: BackstagePassesEvaluator()
-        }
+        self.item_evaluator_factory = ItemEvaluatorFactory()
 
     def update_quality(self):
         for item in self.items:
-            if item.name in self.evaluator_lookup.keys():
-                self.evaluator_lookup[item.name].evaluate(item)
-            else:
-                NormalItemEvaluator().evaluate(item)
+            evaluator = self.item_evaluator_factory.create(item.name)
+            evaluator.evaluate(item)
