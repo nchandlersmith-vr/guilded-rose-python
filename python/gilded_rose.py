115d6790421f7a8b09a691e73b46014a922d223d
Nathan Chandler-Smith <nchandlersmith@verticalrelevance.com>
2022-08-11 08:37:19 -0500

    gilded_rose.py reduces number of factories created. Moves constructor out of loop.

--- python/gilded_rose.py
+++ python/gilded_rose.py
@@ -5,9 +5,9 @@ from python.item import Item
 class GildedRose(object):
     def __init__(self, items: [Item], item_evaluator_factory):
         self.items = items
-        self.item_evaluator_factory = item_evaluator_factory
+        self.item_evaluator_factory = item_evaluator_factory()
 
     def update_quality(self) -> None:
         for item in self.items:
-            evaluator = self.item_evaluator_factory().create(item.name)
+            evaluator = self.item_evaluator_factory.create(item.name)
             evaluator.evaluate(item)
