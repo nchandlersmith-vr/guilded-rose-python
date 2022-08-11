756acdae30affca5aaf0b72f01ae3bcd4d9b1b14
Nathan Chandler-Smith <nchandlersmith@verticalrelevance.com>
2022-08-11 07:38:59 -0500

    ItemEvaluatorFactory returns sulfurus.

--- python/item_evaluator_factories/item_evaluator_factory.py
+++ python/item_evaluator_factories/item_evaluator_factory.py
@@ -1,8 +1,13 @@
 from python.item_evaluator_factories.item_evaluator_factory_abstract_class import ItemEvaluatorFactoryAbstractClass
 from python.item_evaluators.item_evaluator import ItemEvaluator
+from python.item_evaluators.sulfuras_evaluator import SulfurasEvaluator
 from python.exceptions.exceptions import UnsupportedItem
 
 
 class ItemEvaluatorFactory(ItemEvaluatorFactoryAbstractClass):
-    def create(self, item_name: str) -> ItemEvaluator:
-        raise UnsupportedItem(item_name)
+    SULFURUS_ITEM_NAME = "Sulfuras, Hand of Ragnaros"
+
+    def create(self, name: str, sell_in: int, quantity: int) -> ItemEvaluator:
+        if name == ItemEvaluatorFactory.SULFURUS_ITEM_NAME:
+            return SulfurasEvaluator()
+        raise UnsupportedItem(name)
--- python/item_evaluator_factories/tests/test_item_evaluator_factory.py
+++ python/item_evaluator_factories/tests/test_item_evaluator_factory.py
@@ -3,6 +3,9 @@ import pytest
 
 from python.exceptions.exceptions import UnsupportedItem
 from python.item_evaluator_factories.item_evaluator_factory import ItemEvaluatorFactory
+from python.item_evaluators.sulfuras_evaluator import SulfurasEvaluator
+
+SULFURUS_ITEM_NAME = "Sulfuras, Hand of Ragnaros"
 
 
 class TestItemEvaluatorFactory:
@@ -10,5 +13,12 @@ class TestItemEvaluatorFactory:
         unknown_item_name = "unknown"
         with pytest.raises(UnsupportedItem) as exception_info:
             factory = ItemEvaluatorFactory()
-            factory.create(unknown_item_name)
+            factory.create(unknown_item_name, 10, 10)
         assert str(exception_info.value) == "Attempted to create evaluator for unsupported item with the name: unknown."
+
+    def test_create_creates_a_sulfurus_evaluator(self):
+        factory = ItemEvaluatorFactory()
+
+        item = factory.create(SULFURUS_ITEM_NAME, 20, 80)
+
+        assert isinstance(item, SulfurasEvaluator)
