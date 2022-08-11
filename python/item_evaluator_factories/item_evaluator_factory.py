a520274138ab638db6a9d9b19eb35b93caf64be9
Nathan Chandler-Smith <nchandlersmith@verticalrelevance.com>
2022-08-11 08:00:27 -0500

    Refactors ItemEvaluatorFactory.

--- python/item_evaluator_factories/item_evaluator_factory.py
+++ python/item_evaluator_factories/item_evaluator_factory.py
@@ -10,12 +10,13 @@ class ItemEvaluatorFactory(ItemEvaluatorFactoryAbstractClass):
     SULFURUS_ITEM_NAME = "Sulfuras, Hand of Ragnaros"
     AGED_BRIE_ITEM_NAME = "Aged Brie"
     BACKSTAGE_PASSES = "Backstage passes to a TAFKAL80ETC concert"
+    lookup = {
+        SULFURUS_ITEM_NAME: SulfurasEvaluator(),
+        AGED_BRIE_ITEM_NAME: AgedBrieEvaluator(),
+        BACKSTAGE_PASSES: BackstagePassesEvaluator()
+    }
 
     def create(self, name: str) -> ItemEvaluator:
-        if name == ItemEvaluatorFactory.SULFURUS_ITEM_NAME:
-            return SulfurasEvaluator()
-        if name == ItemEvaluatorFactory.AGED_BRIE_ITEM_NAME:
-            return AgedBrieEvaluator()
-        if name == ItemEvaluatorFactory.BACKSTAGE_PASSES:
-            return BackstagePassesEvaluator()
-        raise UnsupportedItem(name)
+        if name not in ItemEvaluatorFactory.lookup.keys():
+            raise UnsupportedItem(name)
+        return ItemEvaluatorFactory.lookup[name]
